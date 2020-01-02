044c872a3a869cd6f47a22339ed6e76b45d59474
Tianna Mosby <mosbyts@vcu.edu>
2020-01-02 17:42:35 -0500

    Updates

---
+++ precache-manifest.42b4713c2a76116645cf23f0ab1e1123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02961ad4bb878d303a62bcda63ce10d6",
+    "url": "/crystal-clicker-game-2/index.html"
+  },
+  {
+    "revision": "17b0012c790716bddbf2",
+    "url": "/crystal-clicker-game-2/static/css/main.8be63f6f.chunk.css"
+  },
+  {
+    "revision": "fbbb6de8e3c721529cb8",
+    "url": "/crystal-clicker-game-2/static/js/2.5b50efb9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/crystal-clicker-game-2/static/js/2.5b50efb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17b0012c790716bddbf2",
+    "url": "/crystal-clicker-game-2/static/js/main.047e5287.chunk.js"
+  },
+  {
+    "revision": "8c70cdd30c1df7a2edf4",
+    "url": "/crystal-clicker-game-2/static/js/runtime-main.54063424.js"
+  }
+]);
